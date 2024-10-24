aa0ec05f8052920cc43db643b0cc813aefc3fda8
caio2983 <caioluna2002@gmail.com>
2024-10-24 15:20:09 -0300

    chore: change from bootstrap carousel to primeng carousel

--- anime-tracker/src/components/slider/slider.component.ts
+++ anime-tracker/src/components/slider/slider.component.ts
@@ -5,6 +5,7 @@ import {
   ElementRef,
   ViewChild,
   Renderer2,
+  OnInit,
 } from '@angular/core';
 import { TrendingResponse } from '../../models/trending-models.model';
 import { CommonModule, NgFor } from '@angular/common';
@@ -15,41 +16,106 @@ import {
   NgbSlideEventSource,
   NgbCarouselConfig,
 } from '@ng-bootstrap/ng-bootstrap';
+
 import { CarouselModule } from 'primeng/carousel';
+import { ButtonModule } from 'primeng/button';
+import { TagModule } from 'primeng/tag';
 
 import { AfterViewChecked } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+interface Car {
+  id?: string;
+  name?: string;
+  description?: string;
+  price?: number;
+}
 
 @Component({
   selector: 'app-slider',
   standalone: true,
-  imports: [CommonModule, NgFor, NgbCarouselModule],
+  imports: [
+    CommonModule,
+    NgFor,
+    NgbCarouselModule,
+    CarouselModule,
+    ButtonModule,
+    TagModule,
+  ],
   templateUrl: './slider.component.html',
   styleUrl: './slider.component.scss',
-  providers: [NgbCarouselConfig],
+  providers: [],
 })
 export class SliderComponent {
   @Input({ required: true }) trendingData!: TrendingResponse;
   @ViewChild('carouselInner') carouselInner!: ElementRef;
 
+  responsiveOptions: any[] | undefined;
+
+  cars: Car[] = [];
+
   showNavigationArrows = false;
   showNavigationIndicators = false;
   images = [1055, 194, 368].map((n) => `https://picsum.photos/id/${n}/900/500`);
 
-  // Using ngAfterViewChecked to access DOM after the component views are initialized and checked for changes.
-  ngAfterViewInit() {
-    // const carouselInner =
-    //   this.elementRef.nativeElement.querySelector('.carousel-item');
-    // const carouselItem =
-    //   this.elementRef.nativeElement.querySelector('.carousel-item');
-    // if (carouselInner & carouselItem) {
-    //   this.renderer.setStyle(carouselInner, 'display', 'flex');
-    //   this.renderer.setStyle(carouselInner, 'flex-direction', 'flex');
-    //   this.renderer.setStyle(carouselItem, 'display', 'block');
-    //   this.renderer.setStyle(carouselItem, 'margin-right', '0');
-    //   this.renderer.setStyle(carouselItem, 'flex', '0 0 calc(100%/3)');
-    // } else {
-    //   console.warn('carousel-inner não encontrado!');
-    // }
+  ngOnInit() {
+    this.cars = [
+      {
+        id: '1',
+        name: 'Bugatti',
+        description: 'Racing car',
+        price: 800,
+      },
+      {
+        id: '2',
+        name: 'Ferrari',
+        description: 'The Prancing Horse',
+        price: 1500,
+      },
+      {
+        id: '3',
+        name: 'Porsche',
+        description: 'Full spectrum',
+        price: 10000,
+      },
+      {
+        id: '1',
+        name: 'Bugatti',
+        description: 'Racing car',
+        price: 800,
+      },
+      {
+        id: '2',
+        name: 'Ferrari',
+        description: 'The Prancing Horse',
+        price: 1500,
+      },
+      {
+        id: '3',
+        name: 'Porsche',
+        description: 'Full spectrum',
+        price: 10000,
+      },
+      {
+        id: '1',
+        name: 'Bugatti',
+        description: 'Racing car',
+        price: 800,
+      },
+      {
+        id: '2',
+        name: 'Ferrari',
+        description: 'The Prancing Horse',
+        price: 1500,
+      },
+      {
+        id: '3',
+        name: 'Porsche',
+        description: 'Full spectrum',
+        price: 10000,
+      },
+    ];
   }
 
   constructor(
