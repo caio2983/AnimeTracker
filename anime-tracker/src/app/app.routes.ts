2afc56aeb163324cc94ba0979a9d14e7197c0927
caio2983 <caioluna2002@gmail.com>
2024-10-27 23:35:30 -0300

    feat: create anime route, update anime page template + styles

--- anime-tracker/src/app/app.routes.ts
+++ anime-tracker/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AnimepageComponent } from '../pages/animepage/animepage.component';
+import { HomepageComponent } from '../pages/homepage/homepage.component';
 
-export const routes: Routes = [];
+export const routes = [
+  { path: 'anime', component: AnimepageComponent },
+  { path: '', component: HomepageComponent },
+];
--- anime-tracker/src/routes/app.routes.ts
+++
@@ -1,4 +0,0 @@
-import { Routes } from '@angular/router';
-import { AnimepageComponent } from '../pages/animepage/animepage.component';
-
-export const routes = [{ path: 'anime', component: AnimepageComponent }];
