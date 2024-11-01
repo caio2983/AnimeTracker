13a69e5243a75709d23f271ef5fac256c947d089
caio2983 <caioluna2002@gmail.com>
2024-11-01 17:28:30 -0300

    feat: delete anime search component and route

--- anime-tracker/src/app/app.routes.ts
+++ anime-tracker/src/app/app.routes.ts
@@ -1,10 +1,8 @@
 import { Routes } from '@angular/router';
 import { AnimepageComponent } from '../pages/animepage/animepage.component';
 import { HomepageComponent } from '../pages/homepage/homepage.component';
-import { AnimesearchComponent } from '../pages/animesearch/animesearch.component';
 
 export const routes = [
   { path: 'anime/:id', component: AnimepageComponent },
   { path: '', component: HomepageComponent },
-  { path: 'animes/:search', component: AnimesearchComponent },
 ];
