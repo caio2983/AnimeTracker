8c44dfa9ca099cc1c4df1258fc0ca23865b5d113
caio2983 <caioluna2002@gmail.com>
2024-11-08 19:54:35 -0300

    fix: serverless function

--- anime-tracker/api/serverless.js
+++ anime-tracker/api/serverless.js
@@ -4,9 +4,14 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 import Fastify from "fastify";
+import animeRelationShipsRoute from "../backend/routes/animeRelationships";
 const app = Fastify({ logger: true });
 
-app.register(import("../server/app.js"));
+app.register(cors, {
+  origin: "*",
+});
+
+app.register(animeRelationShipsRoute);
 
 export default async (req, res) => {
   await app.ready();
