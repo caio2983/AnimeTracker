51a6568bb02c5d4ef17b5a7b10e0fb3d41d190c8
caio2983 <caioluna2002@gmail.com>
2024-11-08 19:37:38 -0300

    chore: update vercel serverless file

---
+++ anime-tracker/api/serverless.js
@@ -0,0 +1,14 @@
+"use strict";
+
+import * as dotenv from "dotenv";
+dotenv.config();
+
+import Fastify from "fastify";
+const app = Fastify({ logger: true });
+
+app.register(import("../server/app.js"));
+
+export default async (req, res) => {
+  await app.ready();
+  app.server.emit("request", req, res);
+};
