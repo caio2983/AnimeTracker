854f03069cb018af043fdfd163ca9eba48bef8f6
caio2983 <caioluna2002@gmail.com>
2024-11-08 21:08:17 -0300

    .

--- anime-tracker/api/serverless.js
+++ anime-tracker/api/serverless.js
@@ -1,8 +1,5 @@
 "use strict";
 
-import * as dotenv from "dotenv";
-dotenv.config();
-
 import Fastify from "fastify";
 import animeRelationShipsRoute from "../backend/routes/animeRelationships.js";
 const app = Fastify({ logger: true });
